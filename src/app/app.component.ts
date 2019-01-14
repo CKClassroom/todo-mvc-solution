1ca7d90b0d24104e37767d98e544b6f7986a1fea
Kevin Yang <chgc_kevin@hotmail.com>
2019-01-14 23:28:23 +0800

    day 3 final code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,64 @@ export class AppComponent {
   title = 'todomvc';
   newTodoPlaceholder = 'What needs to be done?';
 
-  addTodo(newTodo) {
-    console.log(newTodo.value);
-    newTodo.value = '';
+  newTodo = '';
+  todos: Todo[] = [];
+  maxId = 0;
+  toggleStatus;
+
+  get uncompletedCount() {
+    return this.todos.filter(x => x.isCompleted === false).length;
   }
+
+  // 移除已完成項目
+  clearCompleted() {
+    this.todos = this.todos.filter(x => !x.isCompleted);
+  }
+
+  addTodo() {
+    this.todos.push({
+      id: ++this.maxId,
+      item: this.newTodo,
+      isCompleted: false,
+      isEditing: false
+    });
+    // console.log(this.todos);
+    this.newTodo = '';
+  }
+
+  toggleComplete(todo: Todo) {
+    todo.isCompleted = !todo.isCompleted;
+  }
+
+  // 移除項目方法1
+  removeTodo(idx) {
+    this.todos.splice(idx, 1);
+  }
+  // #region 移除項目方法s
+  // 移除項目方法2
+  // removeTodo(todo) {
+  //   this.todos.splice(this.todos.findIndex(x => x.id === todo.id), 1);
+  // }
+
+  // 移除項目方法3
+  // removeTodo(todo) {
+  //   this.todos = this.todos.filter(x => x.id !== todo.id);
+  // }
+  // #endregion
+
+  // Mark all as complete
+  toggleAllComplete(event) {
+    // console.log(this.toggleStatus);
+    console.dir(event.target.checked);
+    this.todos.forEach(x => (x.isCompleted = true));
+
+    // this.todos = this.todos.map(todo => ({ ...todo, isCompleted: true }));
+  }
+}
+
+export interface Todo {
+  id: number;
+  item: string;
+  isCompleted: boolean;
+  isEditing: boolean;
 }
