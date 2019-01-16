00cc5ed645c2da75c71bda10f94f7205285758ce
Kevin Yang <chgc_kevin@hotmail.com>
2019-01-16 23:45:10 +0800

    利用 filter pipe 來條件顯示 todos 清單

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,12 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Todo } from './app.component';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
+  transform(todos: Todo[], condition?: 'all' | 'active' | 'completed'): any {
+    // console.log(value);
+    // console.log(args);
 
-  transform(value: any, args?: any): any {
-    return null;
+    // show all
+    if (condition === 'all') {
+      return todos;
+    }
+    // TODO: show active (not complete) isCompleted
+    if (condition === 'active') {
+      return todos.filter(todo => !todo.isCompleted);
+    }
+    // TODO: show completed isCompleted
+    if (condition === 'completed') {
+      return todos.filter(todo => todo.isCompleted);
+    }
+    return [];
   }
-
 }
